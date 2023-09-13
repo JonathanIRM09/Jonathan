20b31d0ef0536ec525b2b4b4795ab604cc730cf7
JonathanIRM09 <144813438+JonathanIRM09@users.noreply.github.com>
2023-09-13 07:50:12 -0600

    Update Login.php

--- Login.php
+++ Login.php
@@ -9,14 +9,14 @@ $cont = mysqli_fetch_array($query);
 
 
 if($cont['Id_cargo']==1){   
-    header("location:http://localhost/Jonathan/Admin.php");
+    header("location: ../Jonathan/Admin.php");
 }else
 if($cont['Id_cargo']==2)
 {
-    header("location:http://localhost/Jonathan/Productos.php");
+    header("location: ../Jonathan/Productos.php");
 }
 else
 {
-    header("location:http://localhost/Jonathan/Error.html");
+    header("location: ../Jonathan/Error.html");
 }
 ?>
